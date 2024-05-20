99cb4de107ff9eef4850399207299f0bd3d7e6d3
ES-SAADANI Mohamed <mohamed.essaadani@adria-bt.com>
2024-05-20 10:50:53 +0100

    commit

--- src/main/java/org/essaadani/springbootrediscaching/entities/ProductEntity.java
+++ src/main/java/org/essaadani/springbootrediscaching/entities/ProductEntity.java
@@ -4,10 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
